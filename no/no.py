58625c5bfbbfc7aa6ef504fa1ed022ad73040e26
Allehyani-Youssef <yousef4912@gmail.com>
2021-06-07 18:38:43 +0300

    Make Neural Network file and make model 1

--- no/no.py
+++ no/no.py
@@ -1,14 +1,2 @@
-import sys
-
-# def load_numbers(file_name):
-#   numbers = []
-#   with open(file_name) as f:
-#     for line in f:
-#       numbers.append(int(line))
-#   return numbers
-
-# def is_sorted(values):
-#     for index in range(len(values)-1):
-#         if values[index] > 
-
-print("You entered: ",sys.argv[1], sys.argv[2], sys.argv[3])
+counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
+print(counts.get("quincy"))
